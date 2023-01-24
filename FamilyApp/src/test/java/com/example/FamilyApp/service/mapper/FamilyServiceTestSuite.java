e81d23dd622d0aec3f8926097c2064d960ebbc82
Karol Podsiadlo <karolekk131@gmail.com>
2023-01-24 01:00:56 +0100

    fix testing method

--- FamilyApp/src/test/java/com/example/FamilyApp/service/mapper/FamilyServiceTestSuite.java
+++ FamilyApp/src/test/java/com/example/FamilyApp/service/mapper/FamilyServiceTestSuite.java
@@ -2,8 +2,6 @@ package com.example.FamilyApp.service.mapper;
 
 import com.example.FamilyApp.FamilyAppApplication;
 import com.example.FamilyApp.adapter.AdapterFamilyEntityRepository;
-import com.example.FamilyApp.service.Family;
-import com.example.FamilyApp.service.FamilyMapper;
 import com.example.FamilyApp.service.FamilyService;
 import com.example.FamilyApp.web.dto.FamilyDto;
 import com.example.FamilyApp.web.dto.FamilyMemberDto;
@@ -16,12 +14,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -33,46 +29,7 @@ public class FamilyServiceTestSuite {
 
     @Autowired
     private FamilyService familyService;
-    @Autowired
-    private FamilyMapper familyMapper;
-    @Autowired
-    private AdapterFamilyEntityRepository adapterFamilyEntityRepository;
 
-    @Test
-    void testingDataAggregationMethod() {
-        //given
-        Family family = new Family(
-                "Family",
-                7L,
-                3,
-                2,
-                9,
-                new ArrayList<>()
-        );
-        FamilyMemberDto[] familyMemberDtos = new FamilyMemberDto[3];
-        familyMemberDtos[0] = new FamilyMemberDto("Peter", 25);
-        familyMemberDtos[1] = new FamilyMemberDto("Stevie", 2);
-        familyMemberDtos[2] = new FamilyMemberDto("Brian", 7);
-        //and
-        var mockRepository = mock(AdapterFamilyEntityRepository.class);
-        var mockMapper = mock(FamilyMapper.class);
-        //when
-        when(adapterFamilyEntityRepository.existsById(anyLong())).thenReturn(true);
-        when(familyService.getArrayFamilyMembersFromFamilyMemberDb(anyLong())).thenReturn(familyMemberDtos);
-        when(familyMapper.mapToFamilyFromFamilyEntity(any(),anyLong())).thenReturn(family);
-        // system under test
-        var toTest = new FamilyService(mockRepository,mockMapper,null);
-        //and
-        Family familyAfterDataAggregation = toTest.getFamilyWithMembers(anyLong());
-        //and
-        int sizeShouldBe3 = familyAfterDataAggregation.getFamilyMembersDto().size();
-        FamilyMemberDto shouldBeStevie = familyAfterDataAggregation.getFamilyMembersDto().stream().filter(
-                familyMemberDto -> familyMemberDto.getGivenName().equals("Stevie")).findFirst().get();
-        //then
-        Assertions.assertEquals("Family", familyAfterDataAggregation.getFamilyName());
-        Assertions.assertEquals(3, sizeShouldBe3);
-        Assertions.assertEquals(2, shouldBeStevie.getAge());
-    }
 
     @Test
     @DisplayName("should throw IllegalStateException when we put family id which is not exist in db")
