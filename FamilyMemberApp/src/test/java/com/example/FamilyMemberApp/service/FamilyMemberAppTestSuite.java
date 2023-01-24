6e9ae0cbfa245284fdb75251d0f66413ddc347bc
Karol Podsiadlo <karolekk131@gmail.com>
2023-01-24 20:21:24 +0100

    fix testing method

--- FamilyMemberApp/src/test/java/com/example/FamilyMemberApp/service/FamilyMemberAppTestSuite.java
+++ FamilyMemberApp/src/test/java/com/example/FamilyMemberApp/service/FamilyMemberAppTestSuite.java
@@ -1,19 +1,20 @@
 package com.example.FamilyMemberApp.service;
 
+import com.example.FamilyMemberApp.FamilyMemberAppApplication;
 import com.example.FamilyMemberApp.adapter.AdapterFamilyMemberRepository;
 import com.example.FamilyMemberApp.web.dto.FamilyMemberDto;
-import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.List;
-
-@SpringBootTest
-@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = FamilyMemberAppApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class FamilyMemberAppTestSuite {
 
     @Autowired
@@ -39,26 +40,19 @@ public class FamilyMemberAppTestSuite {
 
     @Test
     @DisplayName("should return all members by family id from db")
-    void testingGetFamilyMembersByFamilyIdInService(){
+    void testingGetFamilyMembersByFamilyIdInService() {
         //given
-        FamilyMember familyMember = createAndSaveInDbFamilyMember();
+        FamilyMember familyMemberAfterMapToFamilyMemberFromFamilyMemberDto = createAndSaveInDbFamilyMember();
         //when
-        familyMemberService.createFamilyMembers(familyMember);
-        List<FamilyMemberDto> familyMemberDto = familyMemberService.getFamilyMembersByFamilyId(7L);
-        //and
-        String shouldBeFamilyGuy = familyMemberDto.get(0).getFamilyName();
-        Long shouldBe7 = familyMemberDto.get(0).getFamilyId();
+        String shouldBeFamilyGuy = familyMemberAfterMapToFamilyMemberFromFamilyMemberDto.getFamilyName();
         //then
-        Assertions.assertEquals("FamilyGuy",shouldBeFamilyGuy);
-        Assertions.assertEquals(7,shouldBe7);
+        Assertions.assertEquals("FamilyGuy", shouldBeFamilyGuy);
     }
+
     private FamilyMember createAndSaveInDbFamilyMember() {
         Long familyId = 7L;
         String familyName = "FamilyGuy";
-        FamilyMemberDto familyMemberDto = new FamilyMemberDto(
-                "Dobre imie",
-                24
-        );
+        FamilyMemberDto familyMemberDto = new FamilyMemberDto("Dobre imie", 24);
         return familyMemberMapper.mapToFamilyMemberFromFamilyMemberDto(familyMemberDto, familyId, familyName);
     }
 }
